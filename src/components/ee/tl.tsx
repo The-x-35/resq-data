0421ff18fc02f469b0465b1e3ace4211fbaf347c
The-x-35 <arpit.k3note@gmail.com>
2024-06-25 14:39:55 +0530

    Improved ee

--- src/components/ee/tl.tsx
+++ src/components/ee/tl.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import rr from '../../../assets/rickroll.jpg';
+import './TL.css'; // Import the CSS file
 
 const TL: React.FC = () => {
   return (
     <div className="easter-egg">
       <h1>Congratulations! You've discovered the first Easter egg!</h1>
       <p>This is a special hidden component.</p>
+      <img src={rr} alt="RickRoll" />
     </div>
   );
 };
