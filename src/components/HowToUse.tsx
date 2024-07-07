45d02b75e45f8f652f740e948754d564d3e69c46
The-x-35 <arpit.k3note@gmail.com>
2024-07-07 18:14:21 +0530

    updated ss

--- src/components/HowToUse.tsx
+++ src/components/HowToUse.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import './HowToUse.css';
+import r from '../../assets/recovery.svg';
 
 const HowToUse: React.FC = () => {
   return (
@@ -7,7 +8,11 @@ const HowToUse: React.FC = () => {
       <h1>How to Use ResQ Data</h1>
       <p>Welcome to the ResQ Data guide. Here you'll find instructions on how to use the application effectively.</p>
       <ul>
-        <li>Step 1: Select a disk from the 'Select Disk' page.</li>
+        <li>Step 1: Select the disk on which you want to perform the operations.
+            If you want to take the disk image backup then select the interval and schedule backup, 
+            else click confirm selection to continue with the data recovery process.
+        </li>
+
         <li>Step 2: Create a disk image from the 'Disk Image' page.</li>
         <li>Step 3: View and select recoverable files from the 'Recoverable Files' page.</li>
         <li>Step 4: Recover selected files from the 'Recovery' page.</li>
